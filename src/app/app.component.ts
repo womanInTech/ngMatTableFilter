8ef68f837cdebafa704205ad540a276aff78b255
Evie <ezouras@hotmail.com>
2021-03-26 11:29:44 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,111 @@
 import { Component } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { remoteDummyData, filterObj } from './app-data'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'mat-table-column-filter';
+
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'name', 'username', 'email', 'phone', 'website', 'status'];
+
+  filterValues = {};
+  filterSelectObj = [...filterObj];
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit() {
+    this.getRemoteData();
+    // Overrride default filter behaviour of Material Datatable
+    this.dataSource.filterPredicate = this.createFilter();
+  }
+
+  // Get Uniqu values from columns to build filter
+  getFilterObject(fullObj, key) {
+    const uniqChk = [];
+    fullObj.filter((obj) => {
+      if (!uniqChk.includes(obj[key])) {
+        uniqChk.push(obj[key]);
+      }
+      return obj;
+    });
+    return uniqChk;
+  }
+
+
+
+
+  // Called on Filter change
+  filterChange(filter, event) {
+    //let filterValues = {}
+
+    this.filterValues[filter.columnProp] = event.target.value.trim().toLowerCase()
+    console.log("filter values are ", this.filterValues)
+    this.dataSource.filter = JSON.stringify(this.filterValues)
+  }
+
+  // Custom filter method fot Angular Material Datatable
+  createFilter() {
+    let filterFunction = function(data: any, filter: string): boolean {
+      let searchTerms = JSON.parse(filter);
+      console.log("data is ", data)
+      let isFilterSet = false;
+      for (const col in searchTerms) {
+        if (searchTerms[col].toString() !== '') {
+          isFilterSet = true;
+        } else {
+          delete searchTerms[col];
+        }
+      }
+
+      console.log(searchTerms);
+
+      let nameSearch = () => {
+        let found = false;
+        if (isFilterSet) {
+          for (const col in searchTerms) {
+            searchTerms[col].trim().toLowerCase().split(' ').forEach(word => {
+              if (data[col].toString().toLowerCase().indexOf(word) != -1 && isFilterSet) {
+                found = true
+              }
+            });
+          }
+          return found
+        } else {
+          return true;
+        }
+      }
+      return nameSearch()
+    }
+    return filterFunction
+  }
+
+
+  // Reset table filters
+  resetFilters() {
+    this.filterValues = {}
+    this.filterSelectObj.forEach((value, key) => {
+      value['modelValue'] = undefined;
+    })
+    this.dataSource.filter = "";
+  }
+
+
+
+  // Get remote serve data using HTTP call
+  getRemoteData() {
+    this.dataSource.data = remoteDummyData;
+    this.filterSelectObj.filter((o) => {
+      o.options = this.getFilterObject(remoteDummyData, o.columnProp);
+    });
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* angular material imports */
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatSelectModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
